ef24b4d5ce07af2cb2733e0163d293bb272f252d
Stefan Walther <swr@qlik.com>
2015-06-07 23:08:19 +0200

    Added gulpfile.js for sense-go

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+'use strict';
+var gulp = require('gulp');
+var senseGo = require('sense-go');
+
+var userConfig = {
+	"packageName": "sense-sheet-navigation"
+};
+
+senseGo.init( gulp, userConfig,  function (  ) {
+
+});
