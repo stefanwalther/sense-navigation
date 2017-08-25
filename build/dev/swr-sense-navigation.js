1c12fa3cf74663ca1a0e8beeebe9bac26f4917a2
hvalenzuela1986 <hvalenzuela1986@gmail.com>
2017-08-24 21:55:30 -0300

    Update swr-sense-navigation.js

--- build/dev/swr-sense-navigation.js
+++ build/dev/swr-sense-navigation.js
@@ -80,16 +80,16 @@ define( [
 							if ( !_.isEmpty( url ) ) {
 								
 								if ( url.startsWith( 'http://' ) || url.startsWith( 'https://' ) ) {
-									if ( same == false) {
+									if ( same === false) {
 										window.open( url );
 									} else {
 										window.open( url ,"_self");
 									}
 								} else {
-									if( same == false) {
-										window.open( 'http://' + url );
+									if( same === false) {
+										window.open( "http://" + url );
 									} else {
-										window.open( 'http://' + url ,"_self");
+										window.open( "http://" + url ,"_self");
 									}
 								}
 							}
