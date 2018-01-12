12b87f5da6b256e0fbed6237e3e7ff3c808c2e5a
Stefan Walther <swr@qlik.com>
2018-01-12 20:16:44 +0100

    test: added button alignment test

--- test/e2e/button-alignment.comp.spec.js
+++ test/e2e/button-alignment.comp.spec.js
@@ -2,21 +2,23 @@
 
 const AppOverviewPage = require('./models/app-overview');
 const timeoutTime = 10000;
-const selectors= {
+const selectors = {
   qvtSheet: '.qvt-sheet'
 };
 
 describe('Button Alignment => ', () => {
 
-  xit('should be equal to the taken screenshot', async () => {
+  it('should be equal to the taken screenshot', async () => {
 
     const appOverview = new AppOverviewPage();
     await appOverview.get('sense-navigation_v1x');
 
     await appOverview.openSheet('Button Alignment');
 
-    await browser.takeImageOf({selector: selectors.qvtSheet});
+    await browser.wait(EC.visibilityOf($(selectors.qvtSheet)), timeoutTime, 'Sheet was not visible');
 
+    return expect(await browser.takeImageOf({selector: selectors.qvtSheet})).to.matchImageOf('button_alignment', 'button_alignment');
   });
 
 });
+
