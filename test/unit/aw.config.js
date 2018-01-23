d0c2e802e1604a0fec2e87ef608e247f8a7d8962
Stefan Walther <swr@qlik.com>
2018-01-22 20:52:01 -0500

    test: experiment on unit test coverage

--- test/unit/aw.config.js
+++ test/unit/aw.config.js
@@ -2,9 +2,5 @@
 
 module.exports = {
   url: 'http://localhost:9676/test/unit/index.html',
-  glob: ['test/unit/*.spec.js'],
-  watchGlob: ['src/*.js'],
-  'instrument.exclude': [
-    '**/*main.js'
-  ]
+  glob: ['test/unit/**/*.spec.js']
 };
