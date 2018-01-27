d33df0a1a3f54636a933f4fd1a35df163a970bc1
Stefan Walther <swr@qlik.com>
2018-01-27 22:15:10 +0100

    test(e2e): extend button alignment tests to different themes

--- test/e2e/button-alignment.spec.js
+++ test/e2e/button-alignment.spec.js
@@ -6,16 +6,28 @@ const selectors = {
 
 describe('Button Alignment => ', () => {
 
-  it('should be equal to the taken screenshot', async () => {
+  it('should be equal to the taken screenshot (Bootstrap v3)', async () => {
 
     const appOverview = new AppOverviewPage();
     await appOverview.get('sense-navigation_v1x');
 
-    await appOverview.openSheet('Button Alignment');
+    await appOverview.openSheet('Button Alignment - Bootstrap v3');
 
     await browser.wait(EC.visibilityOf($(selectors.qvtSheet)), timeoutTime, 'Sheet was not visible');
 
-    return expect(await browser.takeImageOf({selector: selectors.qvtSheet})).to.matchImageOf('button_alignment', 'button_alignment');
+    return expect(await browser.takeImageOf({selector: selectors.qvtSheet})).to.matchImageOf('button_alignment_bootstrap_v3', 'button_alignment');
+  });
+
+  it('should be equal to the taken screenshot (Leonardo UI)', async () => {
+
+    const appOverview = new AppOverviewPage();
+    await appOverview.get('sense-navigation_v1x');
+
+    await appOverview.openSheet('Button Alignment - Leonardo UI');
+
+    await browser.wait(EC.visibilityOf($(selectors.qvtSheet)), timeoutTime, 'Sheet was not visible');
+
+    return expect(await browser.takeImageOf({selector: selectors.qvtSheet})).to.matchImageOf('button_alignment_leonardo_ui', 'button_alignment');
   });
 
 });
