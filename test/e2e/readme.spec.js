9a38a01cceb6fdbd0e6b9bd5d45d08b8d719b24d
Stefan Walther <swr@qlik.com>
2018-02-03 02:17:16 +0100

    test: include readme test for icons

--- test/e2e/readme.spec.js
+++ test/e2e/readme.spec.js
@@ -21,5 +21,19 @@ describe('README => ', () => {
     });
   });
 
+  describe('Button Icons', () => {
+    it('should be equal to the taken screenshot', async () => {
+
+      const appOverview = new AppOverviewPage();
+      await appOverview.get('sense-navigation_v1x');
+
+      await appOverview.openSheet('README:button-icons');
+
+      await browser.wait(EC.visibilityOf($(selectors.qvtSheet)), timeoutTime, 'Sheet was not visible');
+
+      return expect(await browser.takeImageOf({selector: selectors.qvtSheet})).to.matchImageOf('button_icons', 'readme');
+    });
+  });
+
 });
 
