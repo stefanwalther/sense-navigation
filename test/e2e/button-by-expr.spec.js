de7e937dc362f8917622c9579a6c76795f9457ca
Stefan Walther <swr@qlik.com>
2018-02-13 23:53:03 +0100

    test(e2e): button by expr for both bs3 and lui

--- test/e2e/button-by-expr.spec.js
+++ test/e2e/button-by-expr.spec.js
@@ -7,16 +7,16 @@ const selectors = {
 
 describe('Buttons By Expression => ', () => {
 
-  xit('should be equal to the taken screenshot', async () => {
+  it('should be equal to the taken screenshot', async () => {
 
     const appOverview = new AppOverviewPage();
     await appOverview.get('sense-navigation_v1x');
 
-    await appOverview.openSheet('test:button-by-css');
+    await appOverview.openSheet('test:button-style-by-expression');
 
     await browser.wait(EC.visibilityOf($(selectors.qvtSheet)), timeoutTime, 'Sheet was not visible');
 
-    return expect(await browser.takeImageOf({selector: selectors.qvtSheet})).to.matchImageOf('button_by_css_samples', 'button_by_css');
+    return expect(await browser.takeImageOf({selector: selectors.qvtSheet})).to.matchImageOf('button_by_expr_samples', 'button_by_expr');
   });
 
 });
