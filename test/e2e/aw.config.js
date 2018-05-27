daf51a2cbfe1dc644cbb20196160d2f1f38295c3
Stefan Walther <swr@qlik.com>
2018-05-27 22:02:34 +0200

    ci: tests

--- test/e2e/aw.config.js
+++ test/e2e/aw.config.js
@@ -6,14 +6,14 @@ const util = require('util');
 
 module.exports = function initConfig(baseConfig) { // eslint-disable-line func-names
   const config = {
-    baseUrl: 'http://127.0.0.1:9076/sense/app/',
+    baseUrl: 'http://localhost:9076/sense/app/',
     directConnect: true,
     artifactsPath: 'test/e2e/artifacts',
     capabilities: {
       browserName: 'chrome',
       unexpectedAlertBehaviour: 'accept',
       chromeOptions: {
-        args: ['--disable-infobars --window-size=1024x768']
+        args: ['--headless --no-sandbox --disable-gpu --disable-infobars --window-size=1024x768']
       }
     },
     plugins: [
