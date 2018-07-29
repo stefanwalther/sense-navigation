1b8555953ca3b631e8248cf4c4162b02a79339a1
Stefan Walther <swr@qlik.com>
2018-07-30 01:10:55 +0200

    feat(default-github): add comments

--- swr-sense-navigation.js
+++ swr-sense-navigation.js
@@ -1,3 +1,16 @@
+/*
+  Many users don't read the README ;-)
+  Then it happens that they download the entire GitHub repository of this extension
+   and then weird things happen as I created several builds for the extension in the
+   ./build directory, see also here: https://github.com/stefanwalther/sense-extension-install#i-am-getting-error-xyz
+  This all results in frustration:
+   - On the user side: Because it does not work out of the box
+   - On my side: Because I get the same "issues" again an again
+  The solution:
+   - I am providing a "default version" of this extension if somebody downloads and imports the entire repo
+   - Showing a default helping message, what went wrong ...
+ */
+
 /* global define,window */
 define(
   [
